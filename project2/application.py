97db32e5910da4dfb61d88f47c139e15cdc803cf
Joseph Semrai <josephsemrai@gmail.com>
2019-06-18 01:38:29 -0400

    Working chat between channels

--- project2/application.py
+++ project2/application.py
@@ -1,5 +1,7 @@
 import os
+import time
 
+from collections import OrderedDict
 from flask import Flask, session, request, render_template, redirect, url_for, jsonify
 from flask_socketio import SocketIO, emit
 
@@ -8,7 +10,9 @@ class Channel:
         self.name = name
         self.messages = []
 
-    def newMessage(self, sender, content, time):
+    def newMessage(self, user, content, time):
+        message = {"content": content, "user": user, "time":time}
+        self.messages.append(message)
         #deletes messages past the past 100
         while len(self.messages) > 100:
             del(self.messages[0])
@@ -17,23 +21,41 @@ class Channel:
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-channels = []
+channels = OrderedDict()
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+    return render_template("main.html", channels=channels)
+
+
+@app.route("/fetchChannel", methods=["POST"])
+def fetchChannel():
+    requestedChannel = request.form.get("channelName")
+    print(channels[requestedChannel].messages)
+    return jsonify({"success": True, "messages": channels[requestedChannel].messages})
 
 @socketio.on("create channel")
 def createChannel(data):
     channelName = data["channelname"]
     newChannel = Channel(channelName)
-    channels.append(newChannel)
+    channels[channelName] = newChannel;
     print(f"Added channel {channelName}")
+    updateChannels()
+    
+def updateChannels():
     returnChannel = []
-    for channel in channels:
+    for channel in channels.values():
         returnChannel.append(channel.name)
     emit("all channels", {"success": True, "channels": returnChannel}, broadcast=True)
 
+@socketio.on("new message")
+def newMessage(data):
+    messageChannel = data["channel"]
+    messageContent = data["content"]
+    messageUser = data["user"]
+    messageTime = int(time.time())
+    channels[messageChannel].newMessage(user=messageUser, content=messageContent, time=messageTime)
+    emit("chat update", channels[messageChannel].messages[-1], broadcast=True)
 
 
 
--- project2/static/js/main.js
+++ project2/static/js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    var username = "test";
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -15,35 +16,109 @@ document.addEventListener('DOMContentLoaded', () => {
         //dismisses modal and sends info on button press
         document.querySelector('#create-channel-button').onclick = () => {
             //attempts to create the channel
-            socket.emit('create channel', {'channelname' : document.querySelector('#create-channel-name').value});
+            socket.emit('create channel', {
+                'channelname': document.querySelector('#create-channel-name').value
+            });
             $('#channelModal').modal('hide');
             document.querySelector('#create-channel-name').value = "";
         };
 
     };
 
-
     //receives new channel broadcast
     socket.on('all channels', data => {
 
         //removes everything in the list before appending the new list
         let channelList = document.querySelector("#channel-list")
-        while (channelList.firstChild)
-        {
+        while (channelList.firstChild) {
             channelList.removeChild(channelList.firstChild);
         }
 
         //creates the new list
         channels = data.channels;
-        alert(channels);
-        for(let i = 0; i < channels.length; i++)
-        {
-            let element = document.createElement('li');
-                element.innerHTML = channels[i];
-                channelList.append(element);
-        }    
-        
+        for (let i = 0; i < channels.length; i++) {
+            let element = document.createElement('button');
+            element.className = "btn btn-danger channelListButton";
+            element.value = channels[i];
+            element.innerHTML = channels[i];
+            channelList.append(element);
+        }
+        joinChannelButton();
     });
+
+    joinChannelButton();
+
+    function joinChannelButton() {
+        document.querySelectorAll('.channelListButton').forEach(button => {
+            button.onclick = () => {
+                alert("Button clicked!")
+                joinChannel(button.value);
+            };
+        });
+    }
+
+    function joinChannel(channelName) {
+        localStorage.setItem('currentChannel', channelName);
+        document.querySelector('#chatTitle').innerHTML = channelName;
+        //gets messages and other channel data
+        const request = new XMLHttpRequest();
+        request.open('POST', '/fetchChannel');
+
+        //callback
+        request.onload = () => {
+            // Extract JSON data from request
+            const data = JSON.parse(request.responseText);
+            if (data["messages"])
+            {
+                document.querySelector('#chatArea').innerHTML = "";
+                for (var key in data["messages"])
+                {
+                    document.querySelector('#chatArea').append(data["messages"][key]["content"]);
+                    
+                }
+            }
+                
+        }
+
+        const data = new FormData();
+        data.append('channelName', channelName);
+
+        request.send(data);
+    }
+
+    //if in channel, make button depressed
+
+    //send message
+    messageBox = document.querySelector("#messageBox");
+    messageBox.addEventListener("keydown", function (e) {
+        if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+        
+        message = messageBox.value;
+
+        if (message != "") //checks if the message is empty
+        {
+            sendMessage(messageBox.value);
+            messageBox.value = "";
+            //resets messagebox
+        }
+        else
+        {
+            alert("Message not valid. Is it empty?");
+        }
+        
+    }
+    });
+
+    function sendMessage(content) {
+        socket.emit('new message', {'channel': localStorage.getItem('currentChannel'), 'content': content, 'user': username});
+    }
+
+    //upon receiving a message
+    socket.on('chat update', data => {
+        document.querySelector("#chatArea").append(data.content)
+    });
+    
+
     
 
 
